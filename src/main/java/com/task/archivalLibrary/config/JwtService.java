84eb83c5371b11639cc851fcd04ac700a9d84473
xSami2 <saami2206@gmail.com>
2024-09-08 20:10:31 +0300

    v1

--- src/main/java/com/task/archivalLibrary/config/JwtService.java
+++ src/main/java/com/task/archivalLibrary/config/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,8 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-  private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+  @Value("${SECRET_KEY}")
+  private  String SECRET_KEY;
 
   public String extractUsername(String token) {
     return extractClaim(token, Claims::getSubject);
@@ -70,7 +72,13 @@ public class JwtService {
   }
 
   private Key getSignInKey() {
-    byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+    System.out.println(SECRET_KEY);
+    System.out.println(getSECRET_KEY());
+    byte[] keyBytes = Decoders.BASE64.decode(getSECRET_KEY());
     return Keys.hmacShaKeyFor(keyBytes);
+  }
+
+  private String getSECRET_KEY(){
+    return SECRET_KEY;
   }
 }
